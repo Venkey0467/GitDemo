9b2735924db5b7682a30fd998d1e9fce69adff77
Venkey0467 <venkataramana3094@gmail.com>
2023-05-05 23:14:16 -0700

    this is commit from gitcodey guy

---
+++ src/test/java/qaclickacademy/AddUser.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.*;
+
+public class AddUser {
+	
+	@Test
+	public void ForChecking1 ()
+	{
+		System.out.println("Hi Yuvan Surya Sai!");
+		System.out.println("How Are You");
+		System.out.println("Good Morning!");
+	}
+	
+	@Test
+	public void ForChecking2 ()
+	{
+		System.out.println("Welcome To Hyderabad!");
+		System.out.println("Whats Up");
+	}
+}
